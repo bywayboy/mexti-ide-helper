45b8110cadad67ea7223eefc65dfb036710fab85
bywayboy <bywayboy@qq.com>
2022-11-17 11:59:42 +0800

    调整备注

--- src/mexti/MinHeap.php
+++ src/mexti/MinHeap.php
@@ -3,17 +3,22 @@ declare(strict_types=1);
 
 namespace mexti;
 
+/**
+ * 最小堆
+ */
 class MinHeap{
     
     /**
      * 获取内部成员数目
+     * @return int 成员数目
      */
     public function count() : int {
         return 0;
     }
 
     /**
-     * 是否为空 等价于 count() == 0
+     * 判断 MinHeap是否为空.
+     * @return bool
      */
     public function isEmpty() : bool{
         return true;
@@ -21,23 +26,26 @@ class MinHeap{
 
     /**
      * 插入一个成员到最小堆中
+     * @param MinHeapNode $n 要插入的节点
+     * @return bool  成功返回 true 失败会抛出一个异常.
      */
-    public function insert(MinHeapNode $n) : bool|int{
+    public function insert(MinHeapNode $n) : bool{
         return true;
     }
 
     /**
      * 从最小堆中移除一个成员
+     * @param MinHeapNode $n 要调整位置的节点
+     * @return bool 成功返回 true 失败会抛出一个异常.
      */
-    public function erase(MinHeapNode $n) : bool|int{
-        return 0;
+    public function erase(MinHeapNode $n) : bool {
+        return true;
     }
 
     /**
      * 成员键值更新后, 调整在最小堆中的位置.
-     * 返回值:
-     *      true:  调整后 $n 位于堆头部
-     *      false: 调整后 $n 不位于堆头部
+     * @param MinHeapNode $n 要调整位置的节点
+     * @return bool true n 位于头部 false n 不位于头部. 其它情况 抛出异常.
      */
     public function adjust(MinHeapNode $n) : bool{
         return true;
@@ -45,15 +53,17 @@ class MinHeap{
 
     /**
      * 从最小堆中弹出一个成员
+     * @return MinHeapNode
      */
-    public function extract() : MinHeapNode {
+    public function extract() : mixed {
         return new MinHeapNode();
     }
 
     /**
-     * 获取最小堆中下一个将要弹出的成员(并不会弹出),
+     * 获取最小堆中下一个将要弹出的成员(并不会弹出).
+     * @return MinHeapNode
      */
-    public function top() : MinHeapNode{
+    public function top() : mixed{
         return new MinHeapNode();
     }
 }
--- src/mexti/MinHeapNode.php
+++ src/mexti/MinHeapNode.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 namespace mexti;
 
+/**
+ * 最小堆节点
+ */
 abstract class MinHeapNode{
     /**
      * 需要被继承类实现的比较方法
@@ -11,28 +14,31 @@ abstract class MinHeapNode{
 
     /**
      * 是否在 MinHeap池中.
+     * @return bool 
     */
     public function inHeap() : bool{
         return true;
     }
 
-    /*
-        * 获取自身所在的MinHeap
+    /**
+     * 获取自身所在的MinHeap
     */
-    public function getHeap() : ?\mexti\MinHeap
+    public function getHeap() : ?MinHeap
     {
-        return new \mexti\MinHeap();
+        return new MinHeap();
     }
+
     /**
      * 索引更新后调整位置.
-     *      true:  调整后 $n 位于堆头部
-     *      false: 调整后 $n 不位于堆头部
+     * @return bool true n 位于头部 false n 不位于头部. 其它情况 抛出异常.
      */
     public function adjust(): bool{
         return true;
     }
+
     /**
      * 从MinHeap中移除自身
+     * @return bool 成功返回 true. 如果这身不在 MinHeap中 返回 false
     */
     public function erase() : bool{
         return true;
