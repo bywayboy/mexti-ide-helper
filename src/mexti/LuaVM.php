b2349894c15aad498dd07ce9dcb138eadd3da52c
bywayboy <bywayboy@qq.com>
2022-12-17 15:59:53 +0800

    完善注释

--- src/mexti/LuaVM.php
+++ src/mexti/LuaVM.php
@@ -17,6 +17,9 @@ class LuaVM {
 
     /**
      * 调用LUA函数.
+     * @param string $func 函数名称
+     * @param mixed ...$args 要传递给lua函数的参数.
+     * @return mixed 返回值 多个返回值会打包成 array 返回.
      */
     public function call(string $func, ...$args)
     {
@@ -25,6 +28,8 @@ class LuaVM {
 
     /**
      * 设置全局变量
+     * @param string $var 要设置的全局变量名称
+     * @param mixed $value 要设置的变量值, null 表示删除一个变量.
      */
     public function set_global(string $var, mixed $value = null)
     {
