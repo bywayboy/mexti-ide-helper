0d2f83222e6fd43b9fdc993f00bebbfa2e57dc7a
bywayboy <bywayboy@qq.com>
2023-01-13 14:46:10 +0800

    两组四个坐标格式互转函数.

--- src/mexti/Lbs.php
+++ src/mexti/Lbs.php
@@ -14,4 +14,25 @@ class Lbs{
     public static function bd2wgs($lat, $lng) : array {
         return ['lat'=>0, 'lng'=> 0];
     }
+
+    /**
+     * GPS坐标转百度地图坐标
+     */
+    public static function wgs2bd($lat, $lng) : array {
+        return ['lat'=>0, 'lng'=> 0];
+    }
+
+    /**
+     * GPS坐标转国测局坐标
+     */
+    public static function wgs2gcj($lat, $lng) : array {
+        return ['lat'=>0, 'lng'=> 0];
+    }
+
+    /**
+     * 国测局坐标转GPS坐标
+     */
+    public static function gcj2wgs($lat, $lng) : array {
+        return ['lat'=>0, 'lng'=> 0];
+    }
 }
